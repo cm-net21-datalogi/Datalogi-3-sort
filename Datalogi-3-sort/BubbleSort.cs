98870986b997b6446f650c18cf2b5ee7e8ea21a0
David Andersson <lejonmanen@gmail.com>
2022-06-22 11:43:20 +0200

    Add project files.

---
+++ Datalogi-3-sort/BubbleSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalogi_3_sort
+{
+	public class BubbleSort
+	{
+		public static int[] Sort(int[] array)
+		{
+			// TODO
+		}
+	}
+}
