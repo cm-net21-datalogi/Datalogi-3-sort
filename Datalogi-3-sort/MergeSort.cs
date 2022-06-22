80e61209d9ff4d8846ce62b9777e9f41eaf70fab
David Andersson <lejonmanen@gmail.com>
2022-06-22 14:03:11 +0200

    MergeSort preparation

---
+++ Datalogi-3-sort/MergeSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalogi_3_sort
+{
+	public class MergeSort
+	{
+		public static int[] Sort(int[] array)
+		{
+			// TODO
+			throw new NotImplementedException();
+		}
+		public static int[] Merge(int[] first, int[] second)
+		{
+			// TODO
+			throw new NotImplementedException();
+		}
+	}
+}
--- Datalogi-3-sort/Program.cs
+++ Datalogi-3-sort/Program.cs
@@ -3,6 +3,7 @@ using Datalogi_3_sort;
 
 static void TestBubbleSort()
 {
+	Console.WriteLine("BubbleSort test");
     Random random = new Random();
     int[] array = new int[12];
     for (int i = 0; i < array.Length; i++)
@@ -15,5 +16,21 @@ static void TestBubbleSort()
 	{
 		Console.WriteLine(x);
 	}
+	Console.WriteLine("");
 }
-TestBubbleSort();
+//TestBubbleSort();
+
+static void TestMerge()
+{
+	Console.WriteLine("MergeSort test");
+    int[] xs = new int[] { -8, -4, 0, 4, 8, 12 };
+    int[] ys = new int[] { -3, 3, 5, 7, 15 };
+    int[] merged = MergeSort.Merge(xs, ys);
+
+    foreach(var m in merged)
+	{
+		Console.WriteLine(m);
+	}
+	Console.WriteLine("");
+}
+TestMerge();
