7ea8db9e6f7e4b4e354db8012ab8464b62d1b0e3
David Andersson <lejonmanen@gmail.com>
2022-06-22 13:51:50 +0200

    Bubble sort

--- Datalogi-3-sort/BubbleSort.cs
+++ Datalogi-3-sort/BubbleSort.cs
@@ -8,9 +8,35 @@ namespace Datalogi_3_sort
 {
 	public class BubbleSort
 	{
-		public static int[] Sort(int[] array)
+		public static int[] Sort(int[] oldArray)
 		{
-			// TODO
+			// Börja från första elementet.
+			// Från första till sista osorterade* elementet i listan:
+			// Jämför med nästa element. Är de i rätt ordning? I så fall, gå vidare.
+			// Om de inte är i rätt ordning, byt plats.
+			// Gå vidare till nästa element.
+			// När du når slutet på listan, är sista elementet garanterat på rätt plats. Börja om från början.
+			// Upprepa tills alla element är sorterade.
+			int[] array = oldArray.ToArray(); // n
+			
+			for(int j=0; j<array.Length; j++)  // n
+			{
+				for(int i=0; i< array.Length - 1; i++)  // n-1
+				{
+					// Om två element kommer i fel ordning, byt plats på dem
+					if (array[i] > array[i + 1])  // 1
+					{
+						int temp = array[i];  // 1
+						array[i] = array[i + 1];  // 1
+						array[i + 1] = temp;  // 1
+					}
+					// sammanlagt 1-4
+				}
+			}
+			return array;
+			// Tidskomplexitet: n + n * (n-1) * 4
+			// == n + 4 * n^2 - 4*n == 4*n^2 + n - 4*n
+			// --> O(n^2)
 		}
 	}
 }
--- Datalogi-3-sort/Program.cs
+++ Datalogi-3-sort/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Datalogi_3_sort;
+
+static void TestBubbleSort()
+{
+    Random random = new Random();
+    int[] array = new int[12];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 5000);
+    }
+    int[] sorted = BubbleSort.Sort(array);
+
+    foreach(var x in sorted)
+	{
+		Console.WriteLine(x);
+	}
+}
+TestBubbleSort();
