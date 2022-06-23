2f231213f8321912f42057b93f1556aed7a7620d
David Andersson <lejonmanen@gmail.com>
2022-06-23 10:28:24 +0200

    Mergesort

--- Datalogi-3-sort/MergeSort.cs
+++ Datalogi-3-sort/MergeSort.cs
@@ -10,13 +10,63 @@ namespace Datalogi_3_sort
 	{
 		public static int[] Sort(int[] array)
 		{
-			// TODO
-			throw new NotImplementedException();
+			// Om listan är tilräckligt liten och sorterad, avsluta
+			// Annars: dela listan på mitten och sortera varje del för sig
+			// Kombinera de båda sorterade listorna
+
+			if (array.Length == 1)  // 1
+			{
+				return array;
+			}
+			// [10, 11, 12, 13, 14]
+			int[] first = new int[array.Length / 2];  // n/2
+			int[] second = new int[array.Length - first.Length]; // n/2
+			for(int i=0; i<first.Length; i++)  // n/2
+			{
+				first[i] = array[i];
+			}
+			for(int i=0; i<second.Length; i++)  // n/2
+			{
+				second[i] = array[first.Length + i];
+			}
+			first = Sort(first);  // n * log(n)
+			second = Sort(second);  // n * log(n)
+			return Merge(first, second);  // n/2 + n/2 --> O(n)
+			// Sammanlagt: O(n * log(n))
 		}
 		public static int[] Merge(int[] first, int[] second)
 		{
-			// TODO
-			throw new NotImplementedException();
-		}
+			// skapa en ny array för att spara den kombinerade datan
+			// skapa variabler för index
+			// loopa
+			// Tidskomplexitet: m+n + 3 + (m+n)*4 == (m+n)*5 + 3
+			// --> O(m + n)
+
+			int[] merged = new int[first.Length + second.Length]; //m+n
+			int firstIndex = 0, secondIndex = 0, mergedIndex = 0; // 3
+			while(firstIndex < first.Length || secondIndex < second.Length)  // m+n
+			{
+				if (firstIndex >= first.Length)
+				{
+					merged[mergedIndex++] = second[secondIndex++];
+				}
+				else if(secondIndex>= second.Length)
+				{
+					merged[mergedIndex++] = first[firstIndex++];
+				}
+				else
+				{
+					if (first[firstIndex] < second[secondIndex])
+					{
+						merged[mergedIndex++] = first[firstIndex++];
+					}
+					else
+					{
+						merged[mergedIndex++] = second[secondIndex++];
+					}
+				}
+			}
+			return merged;
+		}//Merge
 	}
 }
--- Datalogi-3-sort/Program.cs
+++ Datalogi-3-sort/Program.cs
@@ -33,4 +33,23 @@ static void TestMerge()
 	}
 	Console.WriteLine("");
 }
-TestMerge();
+//TestMerge();
+
+static void TestMergeSort()
+{
+    Console.WriteLine("MergeSort test");
+    Random random = new Random();
+    int[] array = new int[12];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 5000);
+    }
+    int[] sorted = MergeSort.Sort(array);
+
+    foreach (var x in sorted)
+    {
+        Console.WriteLine(x);
+    }
+    Console.WriteLine("");
+}
+TestMergeSort();
